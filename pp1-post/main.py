386c63b0f8d48847e14e7d42574b07dffd7c724b
sebastian.klemkosky <sebastianklemkosky@gmail.com>
2025-02-12 18:58:22 -0600

    added regex

--- pp1-post/main.py
+++ pp1-post/main.py
@@ -1,15 +1,18 @@
 import sys
+import re
 
 BOOLEAN_CONSTANTS = {"true", "false"}
 KEYWORDS = {"void", "int", "double", "bool", "string", "null", "for", "while", "if", "else", "return", "break", "Print", "ReadInteger", "ReadLine"}
 
+TOKEN_PATTERN = re.compile(r"\b(true|false)\b|\b(void|int|double|bool|string|null|for|while|if|else|return|break|Print|ReadInteger|ReadLine)\b|\b\w+\b")
+
 def scan(source_code):
     tokens = []
     for line_num, line in enumerate(source_code.splitlines(), start=1):
-        words = line.split()
-        for word in words:
+        for match in TOKEN_PATTERN.finditer(line):
+            word = match.group()
             token_type = "BOOLEAN_CONSTANT" if word in BOOLEAN_CONSTANTS else ("KEYWORD" if word in KEYWORDS else "IDENTIFIER")
-            tokens.append((word, line_num, 1, len(word), token_type))
+            tokens.append((word, line_num, match.start() + 1, match.end(), token_type))
     return tokens
 
 def main():
